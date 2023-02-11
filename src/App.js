e4adf67afd861cb6c95d605d23709a301118d9dd
Austin Wang <yyaustin@bu.edu>
2023-02-11 14:05:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.name}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  console.log("Hello from App component")
+  const myWords = "Keep the console open all the time"
+  return (
+    <div>
+      <Hello name="Austin" />
+      <p>{myWords}</p>
+    </div>
+  )
+}
+
+export default App
